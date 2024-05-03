1d96680eb84df5da8b2d7f05ab8ddc99c7033d87
Kimia Arfaie <kimiaarfaie@Kimias-MacBook-Pro.local>
2024-05-03 12:14:34 +0200

    Visualization stuff

--- visualization.m
+++ visualization.m
@@ -67,21 +67,46 @@ for i = 1:size(Reflectance, 3)
     end
 end
 %%
-%Visualization
+%Visualization with Normalized
 
-Reflectance = Reflectance(:,:,1:77);
+REF = NormalizedReflectance(:,:,1:77);
 D65 = table2array(readtable("D65.csv")); %21:97
-D65 = D65(21:97,2);
 D65 = D65(21:97,2);
 CMF = readmatrix("AllCMFs.xlsx","Sheet","CIE 1931 2 deg","Range","B9:D85"); 
 
 k = 100/(CMF(:,2)'*D65);
-Reflectance = reshape(Reflectance,[],77);
-XYZ = k*Reflectance*diag(D65)*CMF;
+REF = reshape(REF,[],77);
+XYZ = k*REF*diag(D65)*CMF;
 RGB = xyz2srgb(XYZ);
+mean(RGB,'all')
+max( RGB,[],'all')
+min(RGB,[],'all')
 image = reshape(RGB,1295,900,3);
-figure;imshow(image);
+figure;imshow(uint8(image));
 
+%%
+%Visualization with D50
+
+D50 = readmatrix("Illuminants.xls","Sheet","Illuminant SPDs 5nm","Range","C10:C86"); 
+CMF = readmatrix("AllCMFs.xlsx","Sheet","CIE 1931 2 deg","Range","B9:D85"); 
+
+k = 100/(CMF(:,2)'*D65);
+REF = reshape(REF,[],77);
+XYZ = k*REF*diag(D50)*CMF;
+RGB = xyz2srgb(XYZ);
+mean(RGB,'all')
+max( RGB,[],'all')
+min(RGB,[],'all')
+image2 = reshape(RGB,1295,900,3);
+figure;imshow(uint8(image2));
+%%
+figure;
+subplot(1,2,1)
+imshow(uint8(image))
+title("Visualization under D65")
+subplot(1,2,2)
+imshow(uint8(image2))
+title("Visualization under D50")
 %%
 function output = correctingzero(input)
 
